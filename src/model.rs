38ef5858280a0b83a5563ecca3e5f3241fa1ee33
Timothy Spring <TimothyS@bayportfinance.com>
2025-07-25 16:57:28 +0200

    Expand model
    Some fields taken from top

--- src/model.rs
+++ src/model.rs
@@ -9,6 +9,12 @@ enum ProcessState {
 ///Struct to hold information about processes
 struct ProcessInfo {
     pid: u64,
+    ppid: u64,
     name: String,
-    state: String,
+    command: String,
+    cpu_percent: f64,
+    memory_kb: u64,
+    start_time: u64,
+    state: ProcessState,
+    user: String,
 }
