b62f9064b5c3925b38ca6b95c47da18b97394ce9
Timothy Spring <TimothyS@bayportfinance.com>
2025-08-11 22:32:44 +0200

    Start refactoring caches
    Caches, caches, i like using, i like using, caches, caches

--- src/processes.rs
+++ src/processes.rs
@@ -10,7 +10,7 @@ use crate::model::ProcessInfo;
 use crate::model::ProcessState;
 use users::{Users, UsersCache, get_user_by_uid};
 
-static USER_CACHE: LazyLock<UsersCache> = LazyLock::new(|| UsersCache::new());
+// static USER_CACHE: LazyLock<UsersCache> = LazyLock::new(|| UsersCache::new());
 
 ///Get processes and monitor them
 pub fn get_pids() -> Result<Vec<u64>> {
@@ -28,18 +28,18 @@ pub fn get_pids() -> Result<Vec<u64>> {
     Ok(pids)
 }
 
-pub fn get_process_info() -> Result<Vec<ProcessInfo>> {
+pub fn get_process_info(user_cache: &mut UsersCache) -> Result<Vec<ProcessInfo>> {
     let mut process_info_vec = Vec::new();
 
     for pid in get_pids()? {
-        let process_info = parse_process(pid)?;
+        let process_info = parse_process(pid, user_cache)?;
         process_info_vec.push(process_info);
     }
 
     Ok(process_info_vec)
 }
 
-pub fn parse_process(pid: u64) -> Result<ProcessInfo> {
+pub fn parse_process(pid: u64, user_cache: &mut UsersCache) -> Result<ProcessInfo> {
     let stat = std::fs::read_to_string(format!("/proc/{}/stat", pid))?;
 
     let stat_parts: Vec<&str> = stat.split_whitespace().collect();
@@ -62,7 +62,7 @@ pub fn parse_process(pid: u64) -> Result<ProcessInfo> {
     };
 
     let start_time = stat_parts[21].parse().unwrap_or(0);
-    let user = get_process_user(user_pid).ok_or(anyhow!("Failed to get user"))?;
+    let user = get_process_user(user_pid, user_cache).ok_or(anyhow!("Failed to get user"))?;
     let priority = stat_parts[17].parse().unwrap_or(0);
     let nice = stat_parts[17].parse().unwrap_or(0);
     let num_threads = stat_parts[19].parse().unwrap_or(0);
@@ -72,6 +72,9 @@ pub fn parse_process(pid: u64) -> Result<ProcessInfo> {
         let stime = stat_parts[14].parse().unwrap_or(0);
         utime + stime
     };
+
+    let session_id = stat_parts[5].parse().unwrap_or(0);
+    let terminal = get_terminal(stat_parts[6].parse().unwrap_or(0));
 
     let process_info = ProcessInfo {
         pid: user_pid,
@@ -88,17 +91,25 @@ pub fn parse_process(pid: u64) -> Result<ProcessInfo> {
         num_threads,
         virtual_memory_kb,
         cpu_time_total,
-        session_id: todo!(),
+        session_id,
         terminal: todo!(),
     };
 }
 
-fn get_process_user(pid: u64) -> Option<String> {
+fn get_terminal(terminal_id: i32) -> String {
+    if terminal_id == 0 {
+        "???".to_string()
+    } else {
+        "IMPLEMENT MEEEEEE".to_string()
+    }
+}
+
+fn get_process_user(pid: u64, user_cache: &mut UsersCache) -> Option<String> {
     use std::os::unix::fs::MetadataExt;
     let stat_path = format!("/proc/{pid}/stat");
     let meta_data = std::fs::metadata(stat_path).ok()?;
     let uid = meta_data.uid();
-    if let Some(user) = USER_CACHE.get_user_by_uid(uid) {
+    if let Some(user) = user_cache.get_user_by_uid(uid) {
         return Some(user.name().to_string_lossy().to_string());
     } else {
         return Some(format!("uid:{uid}"));
