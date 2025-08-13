dd637a6691e0906d990321dd9f3c190182064af2
Timothy Spring <TimothyS@bayportfinance.com>
2025-08-13 22:49:06 +0200

    Start working on the output of processes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
+use anyhow::Result;
+use processes::get_process_info;
+use users::UsersCache;
+
 mod app_args;
 mod model;
+mod output;
 mod processes;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<()> {
+    //get all the processes
+    let mut user_cache = UsersCache::new();
+    let processes = get_process_info(&mut user_cache)?;
+    Ok(())
 }
---
+++ src/output.rs
@@ -0,0 +1,5 @@
+use anyhow::Result;
+
+pub fn print_process_info() -> Result<()> {
+    todo!();
+}
